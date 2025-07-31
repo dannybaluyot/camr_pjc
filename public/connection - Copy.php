7d3433473ed75c1250b8efc8f98070105fc2a9af
dannybaluyot <daniloangelo.baluyot@dec.com.ph>
2025-07-31 17:07:09 +0800

    Create connection - Copy.php

---
+++ public/connection - Copy.php
@@ -0,0 +1,17 @@
+<?php
+
+				date_default_timezone_set('Asia/Manila');	
+				$servername = "localhost";
+				$username = "dbuser";
+				$password = '';
+				$dbname = "meter_reading";
+
+				// Create connection
+				$conn = mysqli_connect($servername, $username, $password, $dbname);
+				// Check connection
+				if (!$conn) {
+					die("Connection failed: " . mysqli_connect_error());
+				}
+
+
+?> 
